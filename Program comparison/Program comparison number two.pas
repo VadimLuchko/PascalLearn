800e95ca6ef44a9ae687f58aa7fe5fe0dffa6061
Vadim Luchko <luchok.vadim@gmail.com>
2016-09-16 19:27:30 +0300

    Comparing the program with the cycle

---
+++ Program comparison/Program comparison number two.pas
@@ -0,0 +1,20 @@
+uses CRT;
+
+var
+  a, b, i: Integer;
+  //else - инначе
+  //if - если 
+  //then - цикл 
+begin
+  for i := 1 to 5 do
+  begin
+    Writeln('Comparison number two');
+    Write('Enter A:'); Readln(a);
+    Write('Enter B:'); Readln(b);
+    
+    if(a > b) then Writeln('A>B') else 
+    if(a < b) then Writeln('A<B') else Writeln('A=B');
+  Readkey;
+  end;
+  Readkey;
+end.
